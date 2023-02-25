222a2549ac81883b924ef82c1ad874748e67c65d
Vladimir Kavalerov <vladimir@kavalerov.net>
2023-02-25 14:16:29 +0100

    Create chat.tsx

---
+++ ai-chat/src/pages/chat.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@mantine/core";
+
+export default function Chat() {
+  return (
+    <>
+      <Text size="xl" weight={700}>
+        Chat page
+      </Text>
+    </>
+  );
+}
