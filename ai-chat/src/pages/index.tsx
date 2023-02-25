cc7e9f3c9f1c4554ddc7f002fecbfa12641f38b4
Vladimir Kavalerov <vladimir@kavalerov.net>
2023-02-25 14:19:07 +0100

    Update index.tsx

--- ai-chat/src/pages/index.tsx
+++ ai-chat/src/pages/index.tsx
@@ -1,29 +1,15 @@
 import { Text } from "@mantine/core";
-import { InferGetStaticPropsType } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-export default function Home({
-  basePath,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home() {
   const router = useRouter();
-  console.log("basePath: " + basePath);
+  useEffect(() => {
+    router.push("/chat");
+  }, []);
   return (
     <Text size="xl" weight={700}>
       index page
     </Text>
   );
-}
-
-export async function getStaticProps() {
-  var basePath = "";
-  if (process.env.CF_PAGES_URL !== undefined) {
-    basePath = process.env.CF_PAGES_URL;
-  } else {
-    console.log("No CF_PAGES_URL found");
-  }
-  return {
-    props: { basePath: basePath }, // will be passed to the page component as props
-  };
-  // ...
 }
