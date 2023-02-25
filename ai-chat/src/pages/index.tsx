14decd74799995a81f2d2c264d7ebb90a49d431d
Vladimir Kavalerov <vladimir@kavalerov.net>
2023-02-25 18:46:37 +0100

    Update index design

--- ai-chat/src/pages/index.tsx
+++ ai-chat/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Text } from "@mantine/core";
+import { Button, Stack, Text } from "@mantine/core";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
@@ -6,11 +6,20 @@ export default function Home() {
   const router = useRouter();
   return (
     <>
-      <Text size="xl" weight={700}>
-        Hi, you are on AI test page.
-      </Text>
-      <Button onClick={() => router.push("/chat")}>Chat with AI</Button>
-      <Button onClick={() => router.push("/images")}>Images</Button>
+      <Stack
+        justify="center"
+        spacing="xl"
+        align="center"
+        sx={{
+          height: "100vh",
+        }}
+      >
+        <Text size="xl" weight={700}>
+          Hi, you are on AI test page.
+        </Text>
+        <Button onClick={() => router.push("/chat")}>Chat with AI</Button>
+        <Button onClick={() => router.push("/images")}>Images</Button>
+      </Stack>
     </>
   );
 }
