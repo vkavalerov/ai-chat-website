fadb8b03b28f107d7c700d631955687523e70753
Vladimir Kavalerov <vladimir@kavalerov.net>
2023-02-25 17:13:03 +0100

    Update chat.tsx

--- ai-chat/src/pages/chat.tsx
+++ ai-chat/src/pages/chat.tsx
@@ -31,7 +31,7 @@ export default function Chat(
         }}
       ></Box>
       <Text size="xl" weight={800} align="center">
-        Chat with AI (version 0.0.1)
+        Chat with AI (version 0.0.2)
       </Text>
       <Box
         sx={{
