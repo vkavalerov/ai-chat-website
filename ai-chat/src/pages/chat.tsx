1ac63172de77b8bd7f75a9db794226487c6dc124
Vladimir Kavalerov <vladimir@kavalerov.net>
2023-02-26 19:46:09 +0100

    Update chat.tsx

--- ai-chat/src/pages/chat.tsx
+++ ai-chat/src/pages/chat.tsx
@@ -73,7 +73,8 @@ export default function Chat(
         radius="md"
         marks={[
           { value: 20, label: "20%" },
-          { value: 50, label: "50%" },
+          { value: 40, label: "40%" },
+          { value: 60, label: "60%" },
           { value: 80, label: "80%" },
         ]}
       />
