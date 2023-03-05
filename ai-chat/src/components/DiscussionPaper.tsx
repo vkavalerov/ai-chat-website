3ca703d446123170ccff1d30744551f578789798
Vladimir Kavalerov <vladimir@kavalerov.net>
2023-03-05 23:50:53 +0100

    Update DiscussionPaper.tsx

--- ai-chat/src/components/DiscussionPaper.tsx
+++ ai-chat/src/components/DiscussionPaper.tsx
@@ -1,5 +1,7 @@
-import { Paper, Text, Button, Group } from "@mantine/core";
+import { Paper, Text, Button, Group, ActionIcon } from "@mantine/core";
+import { IconTrash } from "@tabler/icons-react";
 import { useRouter } from "next/router";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 interface DiscussionProps {
   title: string;
@@ -9,6 +11,7 @@ interface DiscussionProps {
 
 export default function DiscussionPaper(props: DiscussionProps) {
   const router = useRouter();
+  const supabaseClient = useSupabaseClient();
   return (
     <Paper
       shadow="xl"
@@ -23,23 +26,47 @@ export default function DiscussionPaper(props: DiscussionProps) {
         {props.title}
       </Text>
       <Group position="apart">
-        <Button
-          size="md"
-          sx={{
-            width: "100px",
-            height: "30px",
-          }}
-          color="cyan"
-          radius="md"
-          onClick={() => {
-            router.push({
-              pathname: "/chat",
-              query: { id: props.id },
-            });
-          }}
-        >
-          Open
-        </Button>
+        <Group position="left">
+          <Button
+            size="md"
+            sx={{
+              width: "100px",
+              height: "30px",
+            }}
+            color="cyan"
+            radius="md"
+            onClick={() => {
+              router.push({
+                pathname: "/chat",
+                query: { id: props.id },
+              });
+            }}
+          >
+            Open
+          </Button>
+          <ActionIcon
+            sx={{
+              width: "30px",
+              height: "30px",
+            }}
+            size="md"
+            radius="md"
+            variant="filled"
+            color="red"
+            onClick={() => {
+              supabaseClient
+                .from("discussions")
+                .delete()
+                .eq("id", props.id)
+                .then((res) => {
+                  if (res.error) throw res.error;
+                  router.reload();
+                });
+            }}
+          >
+            <IconTrash />
+          </ActionIcon>
+        </Group>
         <Text size="md" weight={300}>
           Created at {props.created_at}
         </Text>
